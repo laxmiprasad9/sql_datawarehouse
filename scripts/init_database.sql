981fad627fc51c53a133715e86947d550c2eb6cc
laxmiprasad9 <rao.laxmi632@gmail.com>
2025-03-31 15:27:37 -0400

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,7 +1,7 @@
 USE master:
 GO
 -- Drop and recreate the 'DataWarehouse' database
-IF EXISTS (SELECT 3 FROM sys.databases WHERE name = Databarehouse")
+IF EXISTS (SELECT 3 FROM sys.databases WHERE name = Databarehouse") 
 BEGIN
   ALTER DATABASE Dataarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
   DROP DATABASE Databarehouse;
@@ -9,13 +9,16 @@ END;
 GO
 - Create the 'Databarehouse' database
 CREATE DATABASE DataWarehouse;
-60
+GO
+
 USE DataWarehouse;
 GO
 -- Create Schemas
 CREATE SCHEMA bronze;
 GO
+  
 CREATE SCHEMA silver;
 GO
+
 CREATE SCHEMA gold;
 GO
